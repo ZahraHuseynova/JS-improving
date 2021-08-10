839399674e9f422364f06cbb01e070f098cb7334
Zahra Huseynova <zahra.huseynova@gmail.com>
2021-08-10 11:33:06 +0400

    js-activity

---
+++ script.js
@@ -0,0 +1,14 @@
+let bands = ["30stm","linkin park","queen","beatles","guns'n'roses","coldplay"];
+
+let question = prompt ("Which is your favorite band?");
+
+
+if(bands.indexOf(question.toLowerCase()) >=0 ){
+     alert("YEAH I LOVE THEM!")
+ }else {
+     alert("Nah. They're pretty lame!")
+ }
+
+
+
+
